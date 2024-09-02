7d5b2394576b4f9bfcc836a50895496a3fa80e1a
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-09-02 14:28:11 +0300

    updated basics

---
+++ Lab Material/Lab 2 Dart Fundamentals/Tutorial/basics.dart
@@ -0,0 +1,37 @@
+void main(List<String> args) {
+  // how to declare and use variables
+  int? age;
+  String name = 'John Doe';
+  var height;
+  bool isMarried = false;
+
+  height = "Hello"; // error
+  print(height);
+
+  height = 5.5;
+
+  // // two more
+  // var weight = 70;
+  dynamic salary = 50000;
+
+  salary = "Fifty thousand";
+
+  print(salary);
+
+  // List
+  List<String> names = ['John', 'Doe', 'Jane'];
+  List<dynamic> values = [1, 'two', 3.0];
+
+  // Set
+  Set<String> uniqueNames = {'John', 'Doe', 'Jane'};
+
+  // Map
+  // Map<keyType, ValueType>
+  Map<String, dynamic> person = {
+    'name': 'John Doe',
+    'age': 30,
+    'isMarried': false
+  };
+
+  print(person['name']);
+}

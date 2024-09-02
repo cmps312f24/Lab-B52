f6800f8c8c5dd21953e4fe0f509e19d7f6647b94
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-09-02 16:06:07 +0300

    Create friends.dart

---
+++ Lab Material/Lab 2 Dart Fundamentals/Tutorial/friends.dart
@@ -0,0 +1,17 @@
+class Friends {
+  String name;
+  int age;
+
+  // Friends(String name, int age) {
+  //   this.name = 'John Doe';
+  //   this.age = 30;
+  // }
+
+  Friends(this.name, this.age);
+  
+  @override
+  String toString() {
+    // TODO: implement toString
+    return super.toString();
+  }
+}

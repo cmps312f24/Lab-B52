38fe24810bde6c23580bdff04f31497d406610d2
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-10-10 08:49:18 +0300

    updated

--- Lab Practice/Lab 7 Navigations and State Management/Tutorial/qbanking_app/lib/providers/beneficiary_provider.dart
+++ Lab Practice/Lab 7 Navigations and State Management/Tutorial/qbanking_app/lib/providers/beneficiary_provider.dart
@@ -1,1 +1,30 @@
+import 'dart:convert';
 
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qbanking_app/model/beneficiary.dart';
+
+class BeneficiaryProvider extends Notifier<List<Beneficiary>> {
+  @override
+  List<Beneficiary> build() {
+    initializeState();
+    return [];
+  }
+
+  void initializeState() async {
+    var data = await rootBundle.loadString('assets/data/beneficiaries.json');
+
+    var beneficiariesMap = jsonDecode(data);
+    for (var beneficiaryMap in beneficiariesMap) {
+      addBeneficiary(Beneficiary.fromJson(beneficiaryMap));
+    }
+  }
+
+  void addBeneficiary(Beneficiary beneficiary) {
+    state = [...state, beneficiary];
+  }
+}
+
+final beneficiaryNotifierProvider =
+    NotifierProvider<BeneficiaryProvider, List<Beneficiary>>(
+        () => BeneficiaryProvider());
--- Lab Practice/Lab 7 Navigations and State Management/Tutorial/qbanking_app/lib/providers/transfer_provider.dart
+++ Lab Practice/Lab 7 Navigations and State Management/Tutorial/qbanking_app/lib/providers/transfer_provider.dart
@@ -1,1 +1,42 @@
+import 'dart:convert';
 
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qbanking_app/model/transfer.dart';
+
+class TransferProvider extends Notifier<List<Transfer>> {
+  @override
+  List<Transfer> build() {
+    initializeState();
+    return [];
+  }
+
+  void initializeState() async {
+    var data = await rootBundle.loadString('assets/data/transfers.json');
+
+    var transfersMap = jsonDecode(data);
+    for (var transferMap in transfersMap) {
+      addTransfer(Transfer.fromJson(transferMap));
+    }
+  }
+
+  void addTransfer(Transfer transfer) {
+    state = [...state, transfer];
+  }
+
+  void removeTransfer(Transfer transfer) {
+    state = state
+        .where((element) => element.transferId != transfer.transferId)
+        .toList();
+  }
+
+  void updateTransfer(Transfer transfer) {
+    state = state
+        .map((element) => element.id == transfer.id ? transfer : element)
+        .toList();
+  }
+}
+
+final transferNotifierProvider =
+    NotifierProvider<TransferProvider, List<Transfer>>(
+        () => TransferProvider());
